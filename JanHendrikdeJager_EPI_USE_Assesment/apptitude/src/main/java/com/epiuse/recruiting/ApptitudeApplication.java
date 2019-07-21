359873b317965dcd9c56953a0cf72cfa5731fcb0
LordFirehawk <39730717+LordFirehawk@users.noreply.github.com>
2019-07-21 09:00:55 +0200

    adding the app
    the app seems to have never been added, and is now being added

---
+++ JanHendrikdeJager_EPI_USE_Assesment/apptitude/src/main/java/com/epiuse/recruiting/ApptitudeApplication.java
@@ -0,0 +1,13 @@
+package com.epiuse.recruiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptitudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptitudeApplication.class, args);
+	}
+
+}
